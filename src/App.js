b03a75fe04f0176fcb37a9f19b655dfe82e6e238
Ian Hancock <northroadian@gmail.com>
2021-12-14 06:47:13 -0600

    router set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
-
+import {BrowserRouter, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <nav>
         <h1>My Articles</h1>
       </nav>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function () {
+    return (
+        <div>
+            <h2>Homepage</h2>
+            <p>lorem IMPSHAKJASLKJHFKJHSkjskjhfashfas;kjfsa;kjHF;Kjshfk;as</p>
+        </div>
+    )
+}
