a86c224375f8c88e27a58bc771f7fde887da1874
Ian Hancock <northroadian@gmail.com>
2021-12-14 06:50:28 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css'
 import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact';
+import About  from './pages/About';
+
 function App() {
   return (
     <div className="App">
@@ -7,6 +11,15 @@ function App() {
       <nav>
         <h1>My Articles</h1>
       </nav>
+      <Route path="/">
+        <Home/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
       </BrowserRouter>
     </div>
   );
