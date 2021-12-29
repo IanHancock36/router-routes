5119cd110e9b7043efd6e585a43011a0baa07e59
Ian Hancock <northroadian@gmail.com>
2021-12-29 07:35:02 -0600

    setTimeout

--- src/Article.js
+++ src/Article.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams , useHistory } from 'react-router-dom'
 import { useFetch } from "../hooks/useFetch"
 const Article = () => {
   const { id } = useParams()
   const url = "http://localhost:3000/articles/" + id
   const { data: article, isPending, error } = useFetch(url)
+  const history = useHistory()
+  useEffect (()=> {
+    if (error){
+   // redirect 
+  //history.goBack()
+
+  setTimeout(()=> {
+    history.push("/")
+  },2000)
+    }
+  },[error])
   return (
     <div>
       {isPending && <div> Loading .... </div>}
